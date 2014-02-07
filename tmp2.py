dde66f73d5c256ba974638ed8621aac19497f7d5
Lee Magnusson <lee@magnul.com>
2014-02-07 07:02:38 -0800

    parsing into datetime and time types

--- tmp2.py
+++ tmp2.py
@@ -3,6 +3,7 @@
 from pprint import pprint
 import sys
 from pyparsing import *
+import datetime
 
 # parser = lessgps.Parser('lessgps/data/nmea.yaml')
 print sys.argv[1]
@@ -17,36 +18,61 @@ def to_latlon(s,l,toks):
         return 0
 		
 def to_utc(s,l,toks):
-    return toks and toks[0] or '0'      # todo (Date also)
+	d = dict(toks)
+	if 'Time (UTC)' in d:
+		dt = datetime.datetime.strptime(d['Time (UTC)'],'%H%M%S')
+		return datetime.time(dt.hour,dt.minute,dt.second)
+	else:
+		return None
+    
+def to_datetime(toks):
+	if 'Datetime (UTC)' in dict(toks):
+		di = dict(toks['Datetime (UTC)'])
+		d = datetime.datetime.strptime(di['Year']+di['Month']+di['Day'],'%y%m%d')
+		t = di['Time (UTC)']
+		dt = d.replace(hour=t.hour,minute=t.minute,second=t.second)
+		return dt
+	else:
+		return None
+    #return datetime.datetime(toks['Year'], toks['Month'], toks['Day'])
+def to_dict_value(toks):
+#	print dict(dict(toks).values()[0])
+	return dict(dict(toks).values()[0])
+	
+def to_float(toks):
+	print 'in to_float'
+	print toks
 
 def parse(s):
 	d = Suppress(',')
-	pref = '$'
+	pref = LineStart() + '$'
 	talker = Word(alphas, exact=2)('Talker')
-	r = Word('+-.' + nums)
+	r = Word('+-.' + nums).setParseAction(to_float)
 	ro = Optional(r)
 	gps_quality = oneOf('0 1 2')('GPS Quality')
 	utc = r.setParseAction(to_utc)('Time (UTC)')
 	ns = oneOf('N S')
 	ew = oneOf('E W')
 	lat = (r + d + ns).setParseAction(to_latlon)('Latitude')
-	long = (r + d + ew).setParseAction(to_latlon)('Longitude')
+	lon = (r + d + ew).setParseAction(to_latlon)('Longitude')
 	num_sats = r('Num Sats')
-	altitude_m = Group(r('Value') + d + Literal('M')('Unit'))('Altitude')
-	geoid_sep_m = Group(r('Value') + d + Literal('M')('Unit'))('Geoidal Separation')
-	dgps = Group(ro('Age') + d + ro('Station ID'))('DGPS')
+	altitude_m = (r('Value') + d + Literal('M')('Unit')).setParseAction(to_dict_value)('Altitude')
+	geoid_sep_m = Group(r('Value') + d + Literal('M')('Unit')).setParseAction(to_dict_value)('Geoidal Separation')
+	dgps = Group(ro('Age') + d + ro('Station ID')).setParseAction(to_dict_value)('DGPS')
 	checksum = '*' + Word(nums + 'ABCDEF')('CheckSum')
 	valid = oneOf('A V')
 	selection_mode = oneOf('A')('Selection Mode')
+	arrival = Optional(oneOf('A V'))
+	dt = Group(utc + d + r('Day') + d + r('Month') + d + r('Year')).setParseAction(to_datetime)('Datetime (UTC)')
 	
-	gga = pref + talker + Literal('GGA')('Type') + d + utc + d + lat + d + long + d + gps_quality + d + num_sats + d + r('Dilution') + d + altitude_m + d + geoid_sep_m + d + dgps + checksum
+	gga = pref + talker + Literal('GGA')('Type') + d + utc + d + lat + d + lon + d + gps_quality + d + num_sats + d + r('Dilution') + d + altitude_m + d + geoid_sep_m + d + dgps + checksum
 	#pprint(grammer)
-	gll = pref + talker + Literal('GLL')('Type') + d + lat + d + long + d + utc + d + valid('Valid1') + d + valid('Valid2') + checksum
+	gll = pref + talker + Literal('GLL')('Type') + d + lat + d + lon + d + utc + d + valid('Valid1') + d + valid('Valid2') + checksum
 	gsa = pref + talker + Literal('GSA')('Type') + d + selection_mode + d + r('Mode') + d + ro('Id1') + d + ro('Id2') + d + ro('Id3') + d + ro('Id4') + d + ro('Id5') + d + ro('Id6') + d + ro('Id7') + d + ro('Id8') + d + ro('Id9') + d + ro('Id10') + d + ro('Id11') + d + ro('Id12') + d + ro('PDOP') + d + ro('HDOP') + d + ro('VDOP') + checksum
 	gsv = pref + talker + Literal('GSV')('Type') + d
 	vtg = pref + talker + Literal('VTG')('Type') + d + r('Track Degrees True') + d + 'T' + d + r('Track Degrees Magnetic') + d + 'M' + d + r('Speed Knots') + d + 'N' + d + r('Speed kmh') + d + 'K' + d + 'A' + checksum
-	zda = pref + talker + Literal('ZDA')('Type') + d
-	aam = pref + talker + Literal('AAM')('Type') + d
+	zda = pref + talker + Literal('ZDA')('Type') + d + dt + d + r('Offset') + d + ro('Unknown') + checksum
+	aam = pref + talker + Literal('AAM')('Type') + d + arrival('Arrival Circle') + d + arrival('Perpendicular Past Waypoint') + d + r('Arrival circle radius') + d + 'N' + d + Word(alphanums)('Waypoint ID') + checksum
 	apb = pref + talker + Literal('APB')('Type') + d
 	bod = pref + talker + Literal('BOD')('Type') + d
 	bwc = pref + talker + Literal('BWC')('Type') + d
